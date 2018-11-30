436edb4207123f302ced99b8c98ba3d2f1d57522
Jeffrey Bruggeman <jgbruggy@gmail.com>
2018-11-30 13:42:47 -0500

    minor changes

--- bact_image_proc.py
+++ bact_image_proc.py
@@ -13,7 +13,7 @@ import numpy as np
 from scipy import stats, ndimage
 
 # import image as greyscale
-img=Image.open("Bacteria Dataset/PIL-15_3dayLBCR-3.jpg").convert('L')
+img=Image.open("Bacteria Dataset/PIL-338_3dayLBCR-4.jpg").convert('L')
 # convert to array
 a = np.array(img)
 # save second copy of img array for trimming
@@ -26,24 +26,24 @@ mode_2 = stats.mode(a[:,0])
 # take min of those modes for ideal threshold
 mode_min = min(mode_1[0], mode_2[0])
 # Set all pixels 30 under threshold and 
-a[a > mode_min-10] = 0
+a[a > mode_min-20] = 0
 # check center of mass 1st time
-test1=ndimage.measurements.center_of_mass(a)
+CoM1=ndimage.measurements.center_of_mass(a)
 # trim all 0 rows and columns
 a=a[~np.all(a==0, axis=1)]
 a=a[:,~np.all(a==0, axis=0)]
 # check center of mass second time
-test2=ndimage.measurements.center_of_mass(a)
+CoM2=ndimage.measurements.center_of_mass(a)
 # find dimensions of a for trimming
 dim_a=a.shape
 # show and annotate trimmed image
 plt.imshow(a)
-plt.annotate('Center of Mass', xy=test2, xycoords='data',
+plt.annotate('Center of Mass', xy=CoM2, xycoords='data',
              xytext=(0.5, 0.5), arrowprops=dict(arrowstyle="->"))
 plt.show()
 # Find image shift based on center of mass
-dimension1=test1[0]-test2[0]
-dimension2=test1[1]-test2[1]
+dimension1=CoM1[0]-CoM2[0]
+dimension2=CoM1[1]-CoM2[1]
 # trim original image
 arr_2=arr_1[int(dimension1):int(dim_a[0]+dimension1), int(dimension2):int(dim_a[1]+dimension2)]
 # show trimmed original
